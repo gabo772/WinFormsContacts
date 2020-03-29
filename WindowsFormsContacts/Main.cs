f1019b84fee2fb7815142ff739de335aa8d44ab4
Gabriel Ramirez Lemus <gabriel.ramirez.lemus@gmail.com>
2020-03-29 04:44:51 -0300

    first commit

---
+++ WindowsFormsContacts/Main.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsContacts
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+
+        #region EVENTS
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            openContactDetailsDialog();
+
+        }
+        #endregion
+
+        #region PRIVATE_METHODS
+        private void openContactDetailsDialog()
+        {
+            ContactDetails contact = new ContactDetails();
+            contact.ShowDialog();
+        }
+        #endregion
+
+    }
+}
